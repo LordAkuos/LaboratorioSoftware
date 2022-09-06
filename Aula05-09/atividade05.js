1bc641e780eea31eb83cddf079e60475dd75763f
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-09-05 22:11:58 -0300

    Add files via upload

--- Aula05-09/atividade05.js
+++ Aula05-09/atividade05.js
@@ -1,16 +1,17 @@
 let Carro = {
-    marca: document.getElementById('marca'),
-    modelo: document.getElementById('modelo'),
-    ano: document.getElementById('ano'),
-    cor: document.getElementById('cor'),
-    quilometragem: document.getElementById('km'),
-    valor_fipe: document.getElementById('valor_fipe'),
+    marca: document.getElementById('marca').value,
+    modelo: document.getElementById('modelo').value,
+    ano: document.getElementById('ano').value,
+    cor: document.getElementById('cor').value,
+    quilometragem: document.getElementById('km').value,
+    valor_fipe: document.getElementById('valor_fipe').value,
     anosUtilizacao(){
         let data = new Date();
         if(this.ano < data){
             return data - this.ano;
         }
-        this.setAnosUtilizacao()
+        this.setAnosUtilizacao();
+        this.getAnosUtilizacao();
     },
     valorMercado(){
         if(this.valor_fipe <= 30000){
@@ -22,17 +23,16 @@ let Carro = {
         if (this.valor_fipe > 50000){
             return this.valor_fipe - (this.valor_fipe * (90 / 100));
         }
-        this.setValorMercado()
+        this.setValorMercado();
+        this.getValorMercado();
     }
 };
 
 let botao = document.getElementById('calcular');
 botao.addEventListener('click', function(){
     let carro = new Carro();
-    //carro.setAnosUtilizacao();
-    //carro.setValorMercado();
-    let p = document.createElement('p');
+    let p = document.createElement("p");
     let t = document.createTextNode("Seu veículo tem " + carro.getAnosUtilizacao() + "anos de utilização. E seu valor de mercado é R$" + carro.getValorMercado());
     p.appendChild(t);
-    document.getElementById('calcular').appendChild(p);
+    document.getElementById('resultado').appendChild(p);
 });
