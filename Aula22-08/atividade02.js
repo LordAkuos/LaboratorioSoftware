fe573ebee6e2cacdb553b066d19cff4eafeed75e
LordAkuos <filipeprimodeoliveira@gmail.com>
2022-09-26 20:15:47 -0300

    Aula26-09

--- Aula22-08/atividade02.js
+++ Aula22-08/atividade02.js
@@ -1,3 +1,14 @@
-let p = document.getElementById('horario');
+/*let p = document.getElementById('horario');
 let horario = setTimeout();
-p.document.write($horario);
+p.document.write($horario);*/
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    // add a zero in front of numbers<10
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML = h + ":" + m + ":" + s;
+    t = setTimeout('startTime()', 500);
+}
