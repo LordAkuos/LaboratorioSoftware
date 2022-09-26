d8106b212a3083cdda31847183eac3ee13de4153
LordAkuos <filipeprimodeoliveira@gmail.com>
2022-09-26 20:53:15 -0300

    Update index.js

--- ProjetosReact/jogo_da_velha/src/index.js
+++ ProjetosReact/jogo_da_velha/src/index.js
@@ -12,6 +12,12 @@ root.render(
 );
 
 class Board extends React.Component {
+  renderSquare(i){
+    return <Square value={i}/>;
+  }
+}
+
+class Square extends React.Component {
   render(){
     return (
       <button className="square">
