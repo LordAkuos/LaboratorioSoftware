1f195d7ec75975887752d4dadee1ae3390048ad7
LordAkuos <filipeprimodeoliveira@gmail.com>
2022-09-26 20:50:11 -0300

    Update index.js

--- ProjetosReact/jogo_da_velha/src/index.js
+++ ProjetosReact/jogo_da_velha/src/index.js
@@ -11,6 +11,16 @@ root.render(
   </React.StrictMode>
 );
 
+class Board extends React.Component {
+  render(){
+    return (
+      <button className="square">
+        {this.props.value}
+      </button>
+    );
+  }
+}
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
