73586d3e7be97dd98011549443caa96bbfed984f
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-09-28 13:34:26 -0300

    Update atividade02.js

--- Aula22-08/atividade02.js
+++ Aula22-08/atividade02.js
@@ -1,17 +1,3 @@
-/*let p = document.getElementById('horario');
-let horario = setTimeout();
-p.document.write($horario);*/
-/*function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    // add a zero in front of numbers<10
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('txt').innerHTML = h + ":" + m + ":" + s;
-    t = setTimeout('startTime()', 500);
-}*/
 function mostrarTempo(){
 	let dateTime= new Date();
 	let time = dateTime.toLocaleTimeString();
