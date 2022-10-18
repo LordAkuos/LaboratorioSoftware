dac8f12b90091adb64d751d2d8b944c3843456a8
LordAkuos <filipeprimodeoliveira@gmail.com>
2022-10-17 21:54:45 -0300

    Aula17-10-React-Cores-botão

--- tabuada/src/App.js
+++ tabuada/src/App.js
@@ -3,19 +3,21 @@ import './App.css';
 import Titulo from './Titulo';
 import Titulo2 from './Titulo2';
 import Tabuada from './Tabuada';
+import Cores from './Cores';
 
-//let n = prompt("Digite um número", "5");
+let n = prompt("Digite um número", "5");
 
 function App() {
   return (
     <div>
+      <Cores />
       <Titulo 
         titulo = "Fatec"
         texto = "Faculdade de Tecnologia de Presidente Prudente"
         link = "https://www.fatecpp.edu.br"
       />
       <Titulo2/>
-      <Tabuada numero = {5}/>
+      <Tabuada numero = {n}/>
     </div>
   );
 }
