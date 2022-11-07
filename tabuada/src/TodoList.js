baea47f3ce51624e971d1f5cc76f9b15a693b899
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-11-07 13:59:58 -0300

    Update TodoList.js

--- tabuada/src/TodoList.js
+++ tabuada/src/TodoList.js
@@ -8,7 +8,9 @@ class TodoList extends React.Component {
 
     render(){
         return(
-            <div><h1>Todo List</h1></div>
+            <div>
+                <h1>Todo List</h1>
+            </div>
         );
     }
 }
