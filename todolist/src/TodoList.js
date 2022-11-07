8c84e3d6f8c1ed11db6292292af2c7b82d088701
LordAkuos <filipeprimodeoliveira@gmail.com>
2022-11-07 20:29:26 -0300

    Aula07-11-TodoList

---
+++ todolist/src/TodoList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class TodoList extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <h1>Todo List</h1>
+        );
+    }
+}
+
+export default TodoList;
