9d9d3b302794a65b7a69c58dfe3bf60a284b1bd7
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-11-07 13:46:28 -0300

    Create Contador.js

---
+++ tabuada/src/Contador.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Contador extends React.Component {
+    constructor(props) {
+        super(props);this.state = {segundos:0}
+    }
+}
+
+export default Contador;
