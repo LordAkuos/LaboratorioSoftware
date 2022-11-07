2326a19cf5079ca2cf1a68f739fc72273dd6e63e
LordAkuos <filipeprimodeoliveira@gmail.com>
2022-11-07 19:19:56 -0300

    Aula07-11

--- tabuada/src/TodoList.js
+++ tabuada/src/TodoList.js
@@ -10,6 +10,11 @@ class TodoList extends React.Component {
         return(
             <div>
                 <h1>Todo List</h1>
+                <ul>
+                    {this.props.itens.map(
+                        item => (<li key={item.id}>{item.text}</li>)
+                    )}
+                </ul>
             </div>
         );
     }
--- tabuada/src/index.js
+++ tabuada/src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 //import './index.css';
 import App from './App';
 //import reportWebVitals from './reportWebVitals';
+import TodoList from './TodoList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const Item1 = {
+  id: Date.now(),
+  text: "Item 1"
+}
+const Item2 = {
+  id: Date.now(),
+  text: "Item 2"
+}
+var itens = [Item1, Item2];
 root.render(
   <React.StrictMode>
     <App />
+    <TodoList itens={itens}/>
   </React.StrictMode>
 );
 
