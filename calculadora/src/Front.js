a5ca67349b0543e028357ffe3509ba65d33ec2f5
LordAkuos <filipeprimodeoliveira@gmail.com>
2022-11-07 22:00:58 -0300

    Aula07-11-Calculadora

--- calculadora/src/Front.js
+++ calculadora/src/Front.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './front.css';
 
 class Front extends React.Component{
     constructor(props){
@@ -11,13 +12,13 @@ class Front extends React.Component{
                     <h1>Calculadora Personalizada</h1>
                 </div>
                 <br/>
-                <div>
+                <div  id='calculadora'>
                     <div>
 
                     </div>
-                    <div>
+                    <div class='numero'>
                         <div>
-                            <button>AC</button>
+                            <button class="ac">AC</button>
                         </div>
                         <div>
                             <button>+/-</button>
@@ -27,6 +28,67 @@ class Front extends React.Component{
                         </div>
                         <div>
                             <button>÷</button>
+                        </div>
+                    </div>
+                    <br/>
+                    <br/>
+                    <div class='numero'>
+                        <div>
+                            <button>7</button>
+                        </div>
+                        <div>
+                            <button>8</button>
+                        </div>
+                        <div>
+                            <button>9</button>
+                        </div>
+                        <div>
+                            <button>X</button>
+                        </div>
+                    </div>
+                    <br/>
+                    <br/>
+                    <div class='numero'>
+                        <div>
+                            <button>4</button>
+                        </div>
+                        <div>
+                            <button>5</button>
+                        </div>
+                        <div>
+                            <button>6</button>
+                        </div>
+                        <div>
+                            <button>-</button>
+                        </div>
+                    </div>
+                    <br/>
+                    <br/>
+                    <div class='numero'>
+                        <div>
+                            <button>1</button>
+                        </div>
+                        <div>
+                            <button>2</button>
+                        </div>
+                        <div>
+                            <button>3</button>
+                        </div>
+                        <div>
+                            <button>+</button>
+                        </div>
+                    </div>
+                    <br/>
+                    <br/>
+                    <div class='numero'>
+                        <div>
+                            <button class="zero">0</button>
+                        </div>
+                        <div>
+                            <button>,</button>
+                        </div>
+                        <div>
+                            <button>=</button>
                         </div>
                     </div>
                 </div>
