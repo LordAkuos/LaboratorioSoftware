5bc5dc1ae0e587016a2e8fe40c1b543d5b872c46
LordAkuos <filipeprimodeoliveira@gmail.com>
2022-11-07 21:04:58 -0300

    Aula07-11-Calculadora

---
+++ calculadora/src/Front.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class Front extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div>
+                <div>
+                    <h1>Calculadora Personalizada</h1>
+                </div>
+                <br/>
+                <div style={{backgroudColor: "black"}}>
+                    <div>
+                        
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Front;
